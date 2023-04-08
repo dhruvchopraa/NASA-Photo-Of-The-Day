21dd54604acbb06b537fa43b23b9017d26ffc159
dhruvchopraa <80399278+dhruvchopraa@users.noreply.github.com>
2023-04-09 04:07:05 +0530

    Ready

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Container, Row, Col} from 'react-bootstrap'
-import styled from 'styled-components'
+
 
 const Footer = () => {
     return (
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -6,7 +6,7 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
 import { login } from '../actions/userActions'
-import NASA from './NASA'
+
 const LoginScreen = ({ location, history }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
