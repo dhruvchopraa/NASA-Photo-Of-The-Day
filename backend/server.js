b59121f510436b00d6db7a3fc21b901984f4634d
dhruvchopraa <80399278+dhruvchopraa@users.noreply.github.com>
2023-04-09 14:45:57 +0530

    Ready1

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import morgan from 'morgan'
@@ -21,15 +20,20 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
-
 app.use("/api/users", userRoutes);
 
+const __dirname = path.resolve()
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else{
+  app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+}
 app.use(notFound);
 app.use(errorHandler);
 
