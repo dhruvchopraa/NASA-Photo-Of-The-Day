f1fb028a591190ed3e01689c5f64826d70b99e70
dhruvchopraa <80399278+dhruvchopraa@users.noreply.github.com>
2023-04-11 03:02:30 +0530

    changes

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return (
+        <div className='main-footer'>
+        <div className='footer-middle'>
+        <div className='container'>
+        <div className='row'>
+        <div className='footer-bottom'>
+            <p className='text-xs-center'>
+               <h5 className='tradem'> Created by Dhruv Chopra</h5> 
+            </p>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>       
+    )
+    
+}
+
+export default Footer;
+
